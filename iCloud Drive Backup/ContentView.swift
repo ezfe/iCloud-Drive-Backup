c589ecca78518065ee3c6e1a1b4102f5dabc37e0
Ezekiel Elin <ezekielelin@me.com>
2022-08-27 18:17:06 -0400

    Initial Commit

---
+++ iCloud Drive Backup/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iCloud Drive Backup
+//
+//  Created by Ezekiel Elin on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCloud Drive Backup/iCloud_Drive_BackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloud_Drive_BackupApp.swift
+//  iCloud Drive Backup
+//
+//  Created by Ezekiel Elin on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iCloud_Drive_BackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
